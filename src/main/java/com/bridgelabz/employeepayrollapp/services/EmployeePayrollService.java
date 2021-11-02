70e0dceacdebd722d730da8217e1ef888423cac6
varshini <dreamvarshini@gmail.com>
2021-11-02 14:04:56 +0530

    ADDED: employee payroll service classs

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -0,0 +1,55 @@
+package com.bridgelabz.employeepayrollapp.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+
+@Service
+public class EmployeePayrollService implements IEmployeePayrollService 
+{
+
+	@Override
+	public List<EmployeePayrollData> getEmployeePayrollData() 
+    {
+		List<EmployeePayrollData> empDataList= new ArrayList<>();
+        empDataList.add(new EmployeePayrollData(1,new EmployeePayrollDTO("Pankaj",3000)));
+        return empDataList;
+	}
+
+	@Override
+	public EmployeePayrollData getEmployeePayrollDataById(int empId) 
+    {
+		EmployeePayrollData empData=null;
+        empData= new EmployeePayrollData(1,new EmployeePayrollDTO("Pankaj",30000));
+		return empData;
+	}
+
+	@Override
+	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) 
+    {
+		
+        EmployeePayrollData empData=null;
+        empData= new EmployeePayrollData(1,employeePayrollDTO);
+		return empData;
+	}
+
+	@Override
+	public EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) 
+    {
+		EmployeePayrollData empData=null;
+        empData= new EmployeePayrollData(1,employeePayrollDTO);
+		return empData;
+	}
+
+	@Override
+	public void deleteEmployeePayrollData(int empId) 
+    {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
