ec280369114d46b1b639bedf24954bef1d912530
varshini <dreamvarshini@gmail.com>
2021-11-02 14:04:30 +0530

    ADDED: interface to perform employee payroll services

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/IEmployeePayrollService.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.employeepayrollapp.services;
+import java.util.List;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+
+public interface IEmployeePayrollService 
+{
+
+    List<EmployeePayrollData> getEmployeePayrollData();
+    EmployeePayrollData getEmployeePayrollDataById(int empId);
+    EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO);
+    EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO);
+    void deleteEmployeePayrollData(int empId);
+
+}
