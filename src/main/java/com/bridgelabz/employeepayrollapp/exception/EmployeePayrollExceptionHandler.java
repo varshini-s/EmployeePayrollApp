7006a16ba83e120397cd982c493cbe267eae2823
varshini <dreamvarshini@gmail.com>
2021-11-06 17:00:17 +0530

    MODIFIED: exception handling for date invalid

--- src/main/java/com/bridgelabz/employeepayrollapp/exception/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/exception/EmployeePayrollExceptionHandler.java
@@ -7,14 +7,20 @@ import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionHandler 
 {
+    private static final String message="Exception while processing REST Request";
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public  ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception)
     {
@@ -31,6 +37,15 @@ public class EmployeePayrollExceptionHandler
     public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception)
     {
         ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", exception.getMessage());
+        return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception)
+    {
+        log.error("Invalid Date Format",exception);
+        ResponseDTO responseDTO = new ResponseDTO(message, "Should have date in the format dd MMM yyyy");
         return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
 
     }
