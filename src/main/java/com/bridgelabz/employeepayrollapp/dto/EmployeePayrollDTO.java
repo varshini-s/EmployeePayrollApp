7157da89b0a9ceb24eeb2b99daa4e04cd266d921
varshini <dreamvarshini@gmail.com>
2021-11-06 17:41:04 +0530

    MODIFIED: name validation pattern

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -15,8 +15,7 @@ import lombok.ToString;
 
 public @ToString class EmployeePayrollDTO 
 {
-	// @NotEmpty(message = "Employee name cannot be null")
-    @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$",message = "employee name Invalid")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "employee name Invalid")
 	public String name;
 	
 	@Min(value = 500,message = "Min wage should be more than 50")
